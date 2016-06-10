dbe6c4741f1443f3f5443b69d514c8053300c584
otservme <contato@otserv.me>
2016-06-10 19:20:29 -0300

    first commit, ubuntu 14.04

---
+++ sources/build/cotire/tfs_CXX_prefix.hxx
@@ -0,0 +1,5 @@
+/* cotire.cmake 1.6.9 generated file */
+/* /home/otserv/global1094/sources/build/cotire/tfs_CXX_prefix.hxx */
+#ifdef __cplusplus
+#include "/home/otserv/global1094/sources/build/cotire/tfs_CXX_prefix.cxx"
+#endif
